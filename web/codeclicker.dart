859e0d7e767688d91176a5c0d56ea641fdfb299d
Ole Martin Gjersvik <olem@olem.org>
2014-05-18 15:40:18 +0200

    Can click for lines.

--- web/codeclicker.dart
+++ web/codeclicker.dart
@@ -1,4 +1,19 @@
+import "dart:html";
+
+var loc = 0;
 
 void main() {
+  querySelectorAll(".writeline").onClick.listen(writeLine);
+  update();
+}
 
+writeLine([_]){
+  loc += 1;
+  update();
+}
+
+update(){
+  querySelectorAll(".loc").forEach((Element e){
+    e.text = loc.toString();
+  });
 }
