95ef1293c1d352d0d9003afae176b2c2d45bf294
Ole Martin Gjersvik <olem@olem.org>
2014-05-19 17:12:25 +0200

    Html create metodes.

--- web/src/view.dart
+++ web/src/view.dart
@@ -4,10 +4,13 @@ class View{
   CodeClicker cc;
   
   View(this.cc){
+    var table = querySelector("#factory");
+    table.querySelector("thead").innerHtml = _getTableHead();
+    table.querySelector("tbody").innerHtml = _getTableBody();
+    
     querySelectorAll(".writeline").onClick.listen(cc.click);
     cc.factories.asMap().forEach((int key, Factory fact){
-      var tier = key + 1;
-      querySelectorAll(".buy.tier$tier").onClick.listen((_) => fact.buy());
+      querySelectorAll(".buy.tier$key").onClick.listen((_) => fact.buy());
     });
   }
   
@@ -15,15 +18,39 @@ class View{
     _setText(querySelectorAll(".loc"),cc.loc);
     _setText(querySelectorAll(".locsec"),cc.locs);
     cc.factories.asMap().forEach((int key, Factory fact){
-      var tier = key + 1;
-      _setText(querySelectorAll(".name.tier$tier"), fact.name);
-      _setText(querySelectorAll(".own.tier$tier"), fact.own);
-      _setText(querySelectorAll(".locs.tier$tier"), fact.locs);
-      _setText(querySelectorAll(".output.tier$tier"), fact.output);
+      _setText(querySelectorAll(".name.tier$key"), fact.name);
+      _setText(querySelectorAll(".own.tier$key"), fact.own);
+      _setText(querySelectorAll(".locs.tier$key"), fact.locs);
+      _setText(querySelectorAll(".output.tier$key"), fact.output);
     });
   }
   
   _setText(ElementList list, Object value){
     list.forEach((Element e) => e.text = value.toString());
+  }
+  
+  _getTableHead(){
+    return """<tr>
+        <th>Name</th>
+        <th>Owns</th>
+        <th>Buy</th>
+        <th>loc/s</th>
+        <th>Output</th>
+      </tr>""";
+  }
+  
+  _getTableBody(){
+    var body = "";
+    for( var i = 0; i < cc.factories.length; i += 1){
+      var fact =  cc.factories[i];
+      body += """<tr>
+      <td class="name tier$i"></td>
+      <td class="own tier$i"></td>
+      <td><button class="buy tier$i">Buy</button></td>
+      <td class="locs tier$i"></td>
+      <td class="output tier$i"></td>
+    </tr>""";
+    }
+    return body;
   }
 }
