bfad1684da3e4b46b4eef347d54a21671219be64
Ole Martin Gjersvik <olem@olem.org>
2014-05-24 14:24:35 +0200

    Addet prefix r_ to reacertch ids.

--- web/src/setup.dart
+++ web/src/setup.dart
@@ -3,7 +3,6 @@ part of codeclicker;
 setup(CodeClicker cc){
   var factories = [
     new Factory('tier0',1,10),
-    new Factory('tier0',1,10),
     new Factory('tier1',10,100),
     new Factory('tier2',100,1000),
     new Factory('tier3',1337,31337), // 1337 and 31337
@@ -15,31 +14,33 @@ setup(CodeClicker cc){
   cc.factories.addAll(factories);
 
   var research = [
-    new Research('work_x2_1', 100, () => cc.worker.researchFactor *= 2),
-    new Research('work_x2_2', 500, () => cc.worker.researchFactor *= 2),
-    new Research('work_x2_3', 1000, () => cc.worker.researchFactor *= 2),
-    new Research('work_x4_1', 40000, () => cc.worker.researchFactor *= 4),
-    new Research('work_x4_2', 400000, () => cc.worker.researchFactor *= 4),
-    new Research('work_x4_3', 4000000, () => cc.worker.researchFactor *= 4),
-    new Research('work_x10_1', 100000000, () => cc.worker.researchFactor *= 10),
-    new Research('work_x10_2', 100000000000, () => cc.worker.researchFactor *= 10),
+    new Research('r_work_x2_1', 100, () => cc.worker.researchFactor *= 2),
+    new Research('r_work_x2_2', 500, () => cc.worker.researchFactor *= 2),
+    new Research('r_work_x2_3', 1000, () => cc.worker.researchFactor *= 2),
+    new Research('r_work_x4_1', 40000, () => cc.worker.researchFactor *= 4),
+    new Research('r_work_x4_2', 400000, () => cc.worker.researchFactor *= 4),
+    new Research('r_work_x4_3', 4000000, () => cc.worker.researchFactor *= 4),
+    new Research('r_work_x10_1', 100000000, () => cc.worker.researchFactor *= 10),
+    new Research('r_work_x10_2', 100000000000, () => cc.worker.researchFactor *= 10),
     
-    new Research('work_locs_1', 256, () => cc._workOfLocs += 0.01),
-    new Research('work_locs_1', 2560, () => cc._workOfLocs += 0.015),
-    new Research('work_locs_1', 25600, () => cc._workOfLocs += 0.025),
-    new Research('work_locs_1', 2560000, () => cc._workOfLocs += 0.05),
-    new Research('work_locs_1', 256000000, () => cc._workOfLocs += 0.05),
-    new Research('work_locs_1', 25600000000, () => cc._workOfLocs += 0.1)
+    new Research('r_work_locs_0', 256, () => cc._workOfLocs += 0.01),
+    new Research('r_work_locs_1', 2560, () => cc._workOfLocs += 0.015),
+    new Research('r_work_locs_2', 25600, () => cc._workOfLocs += 0.025),
+    new Research('r_work_locs_3', 2560000, () => cc._workOfLocs += 0.05),
+    new Research('r_work_locs_4', 256000000, () => cc._workOfLocs += 0.05),
+    new Research('r_work_locs_5', 25600000000, () => cc._workOfLocs += 0.1)
   ];
   cc.research.addAll(research);
   
   var achievements = [
-    new Achievement('a_tier0_1', () => factories[0].own >= 1),
-    new Achievement('a_tier0_10', () => factories[0].own >= 10),
-    new Achievement('a_tier0_25', () => factories[0].own >= 25),
-    new Achievement('a_tier0_50', () => factories[0].own >= 50),
-    new Achievement('a_tier0_100', () => factories[0].own >= 100)
+    
   ];
   cc.achievements.addAll(achievements);
- 
+  
+  var factoryAchievements = factories.expand((Factory f){
+    var list = [1, 10, 25, 50, 100];
+    return list.map((int i) => new Achievement('a_${f.id}_$i', () => f.own >= i));
+  });
+
+  cc.achievements.addAll(factoryAchievements);
 }
