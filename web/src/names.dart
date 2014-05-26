2327652e7d5e03150acd28e5f8cd38838697e58a
Ole Martin Gjersvik <olem@olem.org>
2014-05-26 10:37:13 +0200

    Names for work bonus research.

--- web/src/names.dart
+++ web/src/names.dart
@@ -14,6 +14,15 @@ class Names{
    _n['tier6'] = 'AIs';
    _n['tier7'] = 'Cosmological Computers';
    
+   _n['r_work_x2_0'] = 'Double Spaced (2x lines per click)';
+   _n['r_work_x2_1'] = 'Copy Paste (2x lines per click)';
+   _n['r_work_x2_2'] = 'Auto complete (2x lines per click)';
+   _n['r_work_x4_0'] = 'Compiler (4x lines per click)';
+   _n['r_work_x4_1'] = 'Macros (4x lines per click)';
+   _n['r_work_x4_2'] = 'Code Generation (4x lines per click)';
+   _n['r_work_x10_0'] = 'Viratual Macine (10x lines per click)';
+   _n['r_work_x10_1'] = 'Functinal Programming (10x lines per click)';
+ 
    _n['a_tier0_1'] = 'Punch out (First Punch Card).';
    _n['a_tier0_10'] = 'Just enough for hallo world (10 Punch Card).';
    _n['a_tier0_25'] = 'Mainframe (25 Punch Card).';
