401d3e76c4b952de6c74574281afbff05e373ae0
Ole Martin Gjersvik <olem@olem.org>
2014-09-27 16:09:10 +0200

    Splitt the number.

--- web/elem/loc_counter.dart
+++ web/elem/loc_counter.dart
@@ -4,15 +4,22 @@ import 'dart:html';
 @CustomTag('loc-counter')
 class LocCounterElement extends PolymerElement {
   @published int locs = 0;
-  @published String elem = '';
+  @observable List<String> chars = new ObservableList(24);
 
-  LocCounterElement.created() : super.created();
-  
-  void attached() {
-    shadowRoot.text = '$locs locs';
+  LocCounterElement.created() : super.created(){
+    chars.fillRange(0, 24, '0');
+    chars[21] = 'L';
+    chars[22] = 'o';
+    chars[23] = 'c';
   }
   
-  void locsChanged() {
-    shadowRoot.text = '$locs locs';
+  void attached() => _setPrintLocs();
+  
+  void locsChanged() => _setPrintLocs();
+  
+  _setPrintLocs(){
+    var list = locs.toRadixString(10).split('').toList();
+    list.insertAll(0, new List.filled(21 - list.length, '0'));
+    chars.setRange(0, 21, list);
   }
 }
