45cc7c11c6af0bbdbf559bfc29288f3ccfc1f7f5
Ole Martin Gjersvik <olem@olem.org>
2014-10-06 16:15:09 +0200

    Resize event.

--- web/src/game_loop.dart
+++ web/src/game_loop.dart
@@ -2,15 +2,20 @@ part of codeclicker;
 
 class GameLoop{
   Stream onRender;
+  bool hasResized = false;
+  
+  
   StreamController _render = new StreamController.broadcast(sync: true);
   
   GameLoop(){
     onRender = _render.stream;
+    window.onResize.listen((_) => hasResized = true);
     window.animationFrame.then(_loop);
   }
   
   _loop(_){
-    _render.add(null);
+    _render.add(this);
+    hasResized = false;
     window.animationFrame.then(_loop);
   }
 }
