5f3d3b20291650f2ebc22b304e8cc381ab32d910
Ole Martin Gjersvik <olem@olem.org>
2014-10-07 13:13:35 +0200

    Number resize on window resize.
    There is a bug on first setup.

--- web/src/loc_counter_view.dart
+++ web/src/loc_counter_view.dart
@@ -9,19 +9,34 @@ class LocCounterView {
   List<MatrixDigitView> _digits;
 
   LocCounterView(this.elem) {
+    _updateBoxWidth();
     _digits = elem.querySelectorAll('canvas').map((e) => new MatrixDigitView(e)).toList();
+    
+    elem.style.display = 'none';
+    elem.offsetHeight;
+    elem.style.display = '';
     
     chars[21] = 'L';
     chars[22] = 'o';
     chars[23] = 'c';
   }
   
-  void render(_){
+  void render(GameLoop loop){
+    if(loop.hasResized){
+      _updateBoxWidth();
+    }
+    
     var list = cc.loc.toRadixString(10).split('').toList();
     list.insertAll(0, new List.filled(21 - list.length, '0'));
     chars.setRange(0, 21, list);
     
     new Map.fromIterables(chars, _digits).forEach((c, MatrixDigitView m) => m.char = c);
-    _digits.forEach((m)=> m.render(_));
+    _digits.forEach((m)=> m.render(loop));
+  }
+  
+  _updateBoxWidth(){
+    var boxWidth = (elem.clientWidth / 90).floor() * 10;
+    print(boxWidth);
+    elem.querySelectorAll('.box').style.width = '${boxWidth}px';
   }
 }
